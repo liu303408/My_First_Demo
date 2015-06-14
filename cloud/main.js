21d591fb6f02c91ce254bdcfa57adcbbe300ac7f
liu_303408 <liu_303408@163.com>
2015-06-14 22:01:19 +0800

    add function

--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,24 @@ require("cloud/app.js");
 AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+AV.Cloud.define("sendRequest", function(request, response) {
+AV.Cloud.httpRequest({
+  url: 'http://www.baidu.com/',
+  success: function(httpResponse) {
+    console.log(httpResponse.text);
+    response.success(httpResponse.text);
+  },
+  error: function(httpResponse) {
+    console.error('Request failed with response code ' + httpResponse.status);
+  }
+});
+});
+AV.Cloud.define("queryData", function(request, response) {
+var file = new AV.File('test111.txt', new Buffer('hello world'));
+file.save();
+var jobApplication = new AV.Object("JobApplication");
+jobApplication.set("applicantName", "dandan");
+jobApplication.set("applicantResumeFile", file);
+jobApplication.save();
+});
